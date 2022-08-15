602ca87fbab357e2cdfb13681a0e1d6d23e7782e
Angel Oni <jesuz194@gmail.com>
2022-08-15 15:26:52 -0400

    CRUD pokemon and mongodb

---
+++ src/main.ts
@@ -0,0 +1,16 @@
+import { ValidationPipe } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  // para q sea un prefix global 
+  // http://localhost:3000/api/v2/pokemon/1
+  app.setGlobalPrefix('api/v2')
+  app.useGlobalPipes(new ValidationPipe({
+    whitelist:true,
+    forbidNonWhitelisted:true
+  }))
+  await app.listen(3000);
+}
+bootstrap();
