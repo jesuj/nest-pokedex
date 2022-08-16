99da376742390ee020c8e3e7fe4c5512c6a13543
Angel Oni <jesuz194@gmail.com>
2022-08-16 08:53:00 -0400

    config env

--- README.md
+++ README.md
@@ -16,11 +16,20 @@ npm i -g @nestjs/cli
 ```
 docker-compose up -d
 ```
-5. Reconstruir la base de datos con la semilla(por defecto 600 pokemones)
+5. Clonar el archivo __.env.example__ y renombrar la copia a ```.env```
+
+6. Llenar las variables de entorno definidas en el ```.env```
+
+7. Ejecutar la aplicacion en dev:
+```
+yarn start:dev
+```
+
+9. Reconstruir la base de datos con la semilla(por defecto 600 pokemones)
 ```
 http://localhost:3000/api/v2/seed
 ```
-6. Especificar una determinada cantidad de seed (Maximo 100000)
+10. Especificar una determinada cantidad de seed (Maximo 100000)
 ```
 http://localhost:3000/api/v2/seed?limit=100
 ```
--- src/app.module.ts
+++ src/app.module.ts
@@ -5,16 +5,28 @@ import { join } from 'path';
 import { PokemonModule } from './pokemon/pokemon.module';
 import { CommonModule } from './common/common.module';
 import { SeedModule } from './seed/seed.module';
+import { ConfigModule } from '@nestjs/config';
+import { EnvConfiguration } from './config/env.config';
+import { JoiValidationSchema } from './config/joi.validation';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      load: [EnvConfiguration],
+      validationSchema: JoiValidationSchema,
+    }),
     ServeStaticModule.forRoot({
       rootPath:join(__dirname,'..','public'),
     }),
-    MongooseModule.forRoot('mongodb://localhost:27017/nest-pokemon'),
+    MongooseModule.forRoot(process.env.MONGODB),
     PokemonModule,
     CommonModule,
     SeedModule
   ],
 })
-export class AppModule {}
+export class AppModule {
+  // constructor(){
+  //   // variables de entorno q se esta ejecutando
+  //   console.log(process.env)
+  // }
+}
